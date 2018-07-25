6a73d678011efe5091523bc6c1eab63e57ebfc1d
Eryl <eryl.murphy@gmail.com>
2018-07-25 15:46:27 -0400

    files

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4567;
-const Building = require('./models/Building')
+const Building = require('./models/Building');
 
 app.get('/buildings.json', (request, response) => {
   Building.all()
